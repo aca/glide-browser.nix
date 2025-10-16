b4a7237f4328b7a0928a298996e4f18b0075da2c
rok <acadx0@gmail.com>
2025-10-16 19:24:51 +0900

    test

--- flake.nix
+++ flake.nix
@@ -35,28 +35,28 @@
           ...
         }:
         let
-          glide-browser = pkgs.stdenv.mkDerivation rec {
+          glide-browser-unwrapped = pkgs.stdenv.mkDerivation rec {
             pname = "glide-browser";
-            version = "0.1.51a";
+            version = "0.1.53a";
 
             src =
               let
                 sources = {
                   "x86_64-linux" = pkgs.fetchurl {
                     url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.linux-x86_64.tar.xz";
-                    sha256 = "1r8rnbgwhdqm639m5xixpw7b6v55rgjawjia5xp57g0pgyv243vr";
+                    sha256 = "0iz4rr9hs1z2j5wcv61zb4vcvz9si71ln09lmkyyb55dhblshkxm";
                   };
                   "aarch64-linux" = pkgs.fetchurl {
                     url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.linux-aarch64.tar.xz";
-                    sha256 = "0yclrk760bjyss6w466xaaqq34hfrnh98sz1xf15m1hwjxa7l4vv";
+                    sha256 = "1h9g1dx1zkj9qin3v73g1k8lf82rbkpprhdi506xg52a2j2adb8p";
                   };
                   "x86_64-darwin" = pkgs.fetchurl {
                     url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.macos-x86_64.dmg";
-                    sha256 = "15iqc2x0d40s1kjvc0qzkyfgg6vfzbpg0y92r9asbxl2sjmwcc1w";
+                    sha256 = "0ck1vw9s83gdas08pxnl08kdyil4pf5cqi29w7kpq6ygc2h43jfb";
                   };
                   "aarch64-darwin" = pkgs.fetchurl {
                     url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.macos-aarch64.dmg";
-                    sha256 = "1sq6j5siss02m2pg9hv4ahqfrl76xm8w2idbpw75p4vzl2a72yns";
+                    sha256 = "012wdy52202925gdq4x5sqirprfjl6js6qpfbxgpwmqf6zxqmwkk";
                   };
                 };
               in
@@ -69,23 +69,9 @@
             installPhase =
               if pkgs.stdenv.isLinux then
                 ''
-                  mkdir -p $out/bin $out/lib/glide
+                  mkdir -p $out/lib/glide
                   cp -r glide/* $out/lib/glide/
                   chmod +x $out/lib/glide/glide
-
-                  cat > $out/bin/glide <<EOF
-                  #!/bin/sh
-                  cd $out/lib/glide
-                  exec ${pkgs.steam-run}/bin/steam-run ${pkgs.bash}/bin/bash -c "GTK_IM_MODULE=\$GTK_IM_MODULE $out/lib/glide/glide"
-                  EOF
-                  chmod +x $out/bin/glide
-
-                  cat > $out/bin/glide-browser <<EOF
-                  #!/bin/sh
-                  cd $out/lib/glide
-                  exec ${pkgs.steam-run}/bin/steam-run ${pkgs.bash}/bin/bash -c "GTK_IM_MODULE=\$GTK_IM_MODULE $out/lib/glide/glide"
-                  EOF
-                  chmod +x $out/bin/glide-browser
                 ''
               else
                 ''
@@ -99,6 +85,45 @@
               platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
             };
           };
+
+          glide-fhs = pkgs.buildFHSEnv {
+            name = "glide-browser";
+            targetPkgs = pkgs: with pkgs; [
+              glide-browser-unwrapped
+              gtk3
+              gdk-pixbuf
+              glib
+              cacert
+              dbus
+              p11-kit
+              at-spi2-atk
+              at-spi2-core
+              cups
+              libdrm
+              mesa
+              expat
+              alsa-lib
+              nspr
+              nss
+              cacert
+              pango
+              cairo
+              xorg.libX11
+              xorg.libXcomposite
+              xorg.libXdamage
+              xorg.libXext
+              xorg.libXfixes
+              xorg.libXrandr
+              libxkbcommon
+            ];
+            profile = ''
+              export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+              export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+            '';
+            runScript = "${pkgs.bash}/bin/bash -c 'cd ${glide-browser-unwrapped}/lib/glide && exec ./glide \"\$@\"'";
+          };
+
+          glide-browser = if pkgs.stdenv.isLinux then glide-fhs else glide-browser-unwrapped;
         in
         {
           inherit glide-browser;
